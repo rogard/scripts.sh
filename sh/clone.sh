9f37aa757a311814dd8aefec1922c4d6ff21c704
rogard <erwann.rogard@gmail.com>
2026-06-09 23:09:40 -0400

    added target_dir; renamed unique to clone

--- sh/clone.sh
+++ sh/clone.sh
@@ -11,6 +11,7 @@
 #  Option:
 #    --dry-run=true|false
 #    --extension=<extension>
+#    --target-dir=<directory>
 #  Output
 #    <target file>
 #===============================================================================
@@ -20,56 +21,57 @@ set -euo pipefail
 # `true|false` to be inside a subshell `()` o/w `set -e` will stop execution.
 
 # --- CONFIGURATION ------------------------------------------------------------
-fallback_unique_dir="$HOME/unique"
-fallback_unique_log="$HOME/.local/share/unique.log"
+fallback_default_dir="$HOME/1nbox"
+fallback_clone_log="$HOME/.local/share/clone.log"
 fallback_trash_dir="$HOME/.local/share/Trash/files"
 
-array=('unique_dir' 'unique_log' 'trash_dir')
+array=('default_dir' 'clone_log' 'trash_dir')
 
-for name in ${array[@]}; do
+for name in "${array[@]}"; do
     fallback_var="fallback_${name}"
     [[ -z "${!name-}" ]] && declare "$name"="${!fallback_var}"
 done
 
 fallback_recurse_fun() {
     exec "${BASH_SOURCE[0]}" \
-	 --dry-run="$dryrun_flag" \
-	 --extension="$target_ext" \
-	 "$source_file" \
-	 "$@"
+         --dry-run="$dryrun_flag" \
+         --extension="$target_ext" \
+         --target-dir="$target_dir" \
+         "$source_file" \
+         "$@"
 }
-fallback_unique_fun()
-{
+
+fallback_unique_fun() {
     local var
     var=$(mktemp -u)
     echo "${var##*/tmp.}"
 }
 
-fallback_msg_fun()
-{
+fallback_msg_fun() {
     local msg status symb
     status="$1"
     msg="$2"
-    case $status in
-    	0) symb=✅;;
-    	*) symb=❌;;
+
+    case "$status" in
+        0) symb=✅ ;;
+        *) symb=❌ ;;
     esac
+
     printf '%s %s\n' "$symb" "$msg" >&2
 }
 
 # ------------------------------------------------------------------------------
 
-#  array=('unique_dir' 'unique_log' 'trash_dir')
-array=('unique_dir' 'trash_dir')
+array=('default_dir' 'trash_dir')
 
-for name in ${array[@]}; do
+for name in "${array[@]}"; do
     fallback_var="fallback_${name}"
     [[ -z "${!name-}" ]] && declare "$name"="${!fallback_var}"
 done
 
 array=('msg_fun' 'recurse_fun' 'unique_fun')
 
-for name in ${array[@]}; do
+for name in "${array[@]}"; do
     fallback_fun="fallback_${name}"
     if ! declare -F "$name" >/dev/null && declare -F "$fallback_fun" >/dev/null; then
         eval "$(declare -f "$fallback_fun" | sed "s/^$fallback_fun/$name/")"
@@ -78,63 +80,73 @@ done
 
 dryrun_flag='false'
 target_ext=''
+target_dir=''
 args=()
 
 while (( $# > 0 )); do
     case "$1" in
-  	--dry-run=*)
-	    dryrun_flag="${1#*=}"
-	    [[ "$dryrun_flag" =~ ^(true|false)$ ]] || {
-		msg_fun "$?" "invalid argument $dryrun_flag"
-		exit 1
-	    }
-	    shift
+        --dry-run=*)
+            dryrun_flag="${1#*=}"
+            [[ "$dryrun_flag" =~ ^(true|false)$ ]] || {
+                msg_fun "$?" "invalid argument $dryrun_flag"
+                exit 1
+            }
+            shift
+            ;;
+	--target-dir=*)
+            target_dir="${1#*=}"
+            target_dir="${target_dir/#\~/$HOME}"
+            shift
 	    ;;
         --extension=*)
             target_ext="${1#*=}"
             shift
             ;;
         --*)
-	    echo $(false; msg_fun "$?" "unknown option: $1")
+            echo "$(false; msg_fun "$?" "unknown option: $1")"
             exit 1
             ;;
-  	*) args+=("$1")
-  	   shift
-  	   ;;
+        *)
+            args+=("$1")
+            shift
+            ;;
     esac
 done
 
+[[ -n "$target_dir" ]] || target_dir="$default_dir"
+
 count="${#args[@]}"
 (( count == 1 )) || {
-    msg_fun "$?" $(printf '%s positional arguments' "$count")
+    msg_fun "$?" "$(printf '%s positional arguments' "$count")"
     exit 1
 }
+
 source_file="${args[0]}"
 
-if [[ -z "$target_ext" && "$source_file" == *.* ]]
-then
+if [[ -z "$target_ext" && "$source_file" == *.* ]]; then
     target_ext="${source_file##*.}"
     recurse_fun "$@" || {
-	msg_fun "$?" "failed to recurse"
-	exit 1
+        msg_fun "$?" "failed to recurse"
+        exit 1
     }
 fi
 
-[[ -f "${source_file}" ]] || {
+[[ -f "$source_file" ]] || {
     msg_fun "$?" "$source_file is not a file"
     exit 1
 }
 
-if [[ ! -d "$unique_dir" ]]; then
+if [[ ! -d "$target_dir" ]]; then
     msg='Create unique directory %s? [y/n] '
-    read -p "$(printf "$msg" "$unique_dir")" answer
+    read -r -p "$(printf "$msg" "$target_dir")" answer
+
     case "$answer" in
         [yY]*)
-            mkdir -p "$unique_dir" || {
-		msg_fun "$?" $(printf 'failed to create directory %s' "$unique_dir")
-		exit 1
-	    }
-	    recurse_fun "$@"
+            mkdir -p "$target_dir" || {
+                msg_fun "$?" "$(printf 'failed to create directory %s' "$target_dir")"
+                exit 1
+            }
+            recurse_fun "$@"
             ;;
         *)
             false
@@ -144,16 +156,17 @@ if [[ ! -d "$unique_dir" ]]; then
     esac
 fi
 
-if [[ ! -f "$unique_log" ]]; then
+if [[ ! -f "$clone_log" ]]; then
     msg='Create unique log %s? [y/n] '
-    read -p "$(printf "$msg" "$unique_log")" answer
+    read -r -p "$(printf "$msg" "$clone_log")" answer
+
     case "$answer" in
         [yY]*)
-            touch "$unique_log" || {
-		msg_fun "$?" $(printf 'failed to create file %s' "$unique_log")
-		exit 1
-	    }
-	    recurse_fun "$@"
+            touch "$clone_log" || {
+                msg_fun "$?" "$(printf 'failed to create file %s' "$clone_log")"
+                exit 1
+            }
+            recurse_fun "$@"
             ;;
         *)
             false
@@ -163,33 +176,33 @@ if [[ ! -f "$unique_log" ]]; then
     esac
 fi
 
-target_file="${unique_dir%/}/"
+target_file="${target_dir%/}/"
+target_file+="$(unique_fun)"
 
-target_file+=$(unique_fun)
-
-[[ -z "${target_ext}" ]] || {
-    target_file+=".${target_ext}"
-}
+[[ -z "$target_ext" ]] || target_file+=".${target_ext}"
 
 # --- SIDE EFFECT --------------------------------------------------------------
 
-msg_format=$(printf 'copy %s to %s' "$source_file" "$target_file")  
+msg_format=$(printf 'copy %s to %s' "$source_file" "$target_file")
+
 [[ "$dryrun_flag" == 'true' ]] \
     || cp "$source_file" "$target_file" \
+    && printf '%s\t%s\n' "$source_file" "$target_file" >> "$clone_log" \
     || {
-	msg_fun "$?" "$msg_format"
-	exit 1;
+        msg_fun "$?" "$msg_format"
+        exit 1
     }
 
 msg_format=$(printf 'move %s to %s' "$source_file" "$trash_dir")
 trash_path="${trash_dir%/}/$(basename "$source_file")"
+
 [[ "$dryrun_flag" == 'true' ]] \
-    || mv "$source_file" "${trash_path}" \
+    || mv "$source_file" "$trash_path" \
     || {
-	msg_fun "$?" "$msg_format"
-	exit 1
+        msg_fun "$?" "$msg_format"
+        exit 1
     }
 
-echo "${target_file}"
+echo "$target_file"
 
 exit 0
